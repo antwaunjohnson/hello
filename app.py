61f3b288e728087c27636fb31fa8e35be6a43fc3
Antwaun Johnson <antwaun.l.johnson@gmail.com>
2019-12-13 06:10:26 -0700

    Installed flake8 and created first api endpoint with flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
@@ -6,33 +6,45 @@ import os
 app = Flask(__name__)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+
 
 class Guide(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), unique=False)
     content = db.Column(db.String(250), unique=False)
 
-    def __init(self,title, content):
+    def __init__(self, title, content):
         self.title = title
         self.content = content
 
 
 class GuideSchema(ma.Schema):
     class Meta:
-        fields = ('title', 'content') 
+        fields = ('title', 'content')
 
 
-gude_schema = GuideSchema()
+guide_schema = GuideSchema()
 guides_schema = GuideSchema(many=True)
 
-  
 
-@app.route('/')
-def hello():
-    return "Hey Flask"
+@app.route('/guide', methods=["POST"])
+def add_guide():
+    title = request.json['title']
+    content = request.json['content']
+
+    new_guide = Guide(title, content)
+
+    db.session.add(new_guide)
+    db.session.commit()
+
+    guide = Guide.query.get(new_guide.id)
+
+    return guide_schema.jsonify(guide)
 
 
 if __name__ == '__main__':
