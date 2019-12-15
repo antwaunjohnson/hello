ed45bf0b6fe279d50c9487d3ba733f82f72b85b8
Antwaun Johnson <antwaun.l.johnson@gmail.com>
2019-12-14 18:21:46 -0700

    Created new endpoints from querying all guides, and a sigle guide

--- app.py
+++ app.py
@@ -32,6 +32,7 @@ guide_schema = GuideSchema()
 guides_schema = GuideSchema(many=True)
 
 
+# Endpoint to create a new guide
 @app.route('/guide', methods=["POST"])
 def add_guide():
     title = request.json['title']
@@ -44,6 +45,21 @@ def add_guide():
 
     guide = Guide.query.get(new_guide.id)
 
+    return guide_schema.jsonify(guide)
+
+
+# Endpoint to query all guides
+@app.route('/guides', methods=['GET'])
+def get_guides():
+    all_guides = Guide.query.all()
+    result = guides_schema.dump(all_guides)
+    return jsonify(result)
+
+
+# Endpoint for querying a single guide
+@app.route("/guide/<id>", methods=['GET'])
+def get_guide(id):
+    guide = Guide.query.get(id)
     return guide_schema.jsonify(guide)
 
 
